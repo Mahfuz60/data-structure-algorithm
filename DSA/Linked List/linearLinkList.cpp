7ba5ca73c3e3f5286685a3fdc46adc883c8ee911
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-08-23 12:06:10 +0600

    linear linkList

--- DSA/Linked List/linearLinkList.cpp
+++ DSA/Linked List/linearLinkList.cpp
@@ -14,7 +14,7 @@ public:
   }
 };
 
-// Node Insertion
+// Node Insertion Trail
 void insertAtTrail(Node *&head, int val)
 {
   Node *newNode = new Node(val);
@@ -30,6 +30,16 @@ void insertAtTrail(Node *&head, int val)
     temp = temp->Next;
   }
   temp->Next = newNode;
+}
+// Node Insersion at Head
+void insertAtHead(Node *&head, int val)
+{
+  // s1:New Node creation
+  Node *newNode = new Node(val);
+  // s2:Update of newNode->Next
+  newNode->Next = head;
+  // s3:update of head
+  head = newNode;
 }
 // display function
 void linkDisplay(Node *head)
@@ -48,20 +58,25 @@ void linkDisplay(Node *head)
 int main()
 {
   Node *head = NULL;
-  // insertAtTrail(head, 1);
-  // insertAtTrail(head, 5);
-  // insertAtTrail(head, 8);
-  // insertAtTrail(head, 9);
-  // insertAtTrail(head, 12);
   int n;
-  char choice = 'Y';
-  
-  while (toupper(choice) == 'Y')
+  // Choice 1:Insersion at Head
+  // Choice 2:Insersion at Trail
+  cout << "Choice 1:Insersion at Head" << endl
+       << "Choice 2:Insersion at Trail" << endl
+       << "Choice 3:Exit" << endl
+       << endl;
+  int choice = 2;
+
+  while (choice == 1 || choice == 2)
   {
     cout << "Enter the value:";
     cin >> n;
-    insertAtTrail(head, n);
-    cout << "Do you Want to Continue(Y/N):";
+    if (choice == 1)                            
+      insertAtHead(head, n);
+    else if (choice == 2)
+      insertAtTrail(head, n);
+
+    cout << "Next Choice:";
     cin >> choice;
   }
 
