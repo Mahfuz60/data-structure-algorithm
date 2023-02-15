7738775b624faea8eaa9d0d9650dbf637f678df8
mahfuz60 <mahfuzuralam1996@gmail.com>
2023-02-15 23:46:19 +0600

    zigzag traversal

--- DSA/Binary-Search_tree/BST.cpp
+++ DSA/Binary-Search_tree/BST.cpp
@@ -52,60 +52,109 @@ TreeNode *searchBST(TreeNode *root, int value)
     return NULL;
   if (root->value == value)
   {
-    cout<<root->value;
+    // cout << root->value;
     return root;
   }
   else if (value < root->value)
   {
-    cout<<root->value<<"->";
+    // cout << root->value << "->";
     searchBST(root->leftChild, value);
   }
   else
   {
-    cout<<root->value<<"->";
+    // cout << root->value << "->";
     searchBST(root->rightChild, value);
   }
 }
-TreeNode*inOderSuccesser(TreeNode*root){
-  TreeNode*current=root;
-  while(current->leftChild!=NULL){
-    current=current->leftChild;
-  } 
-  return current;
- 
-}
-TreeNode *deleteBST(TreeNode *root,int value)
+TreeNode *inOderSuccesser(TreeNode *root)
 {
-  if(value<root->value){
-    root->leftChild=deleteBST(root->leftChild,value);
+  TreeNode *current = root;
+  while (current->leftChild != NULL)
+  {
+    current = current->leftChild;
+  }
+  return current;
+}
 
-  }else if(value>root->value){
-     root->rightChild=deleteBST(root->rightChild,value);
-
-  }else{
-    //implemantation of 3 cases
-    if(root->leftChild==NULL){ //case-1 and case-2
-      TreeNode*temp=root->rightChild;
+TreeNode *deleteBST(TreeNode *root, int value)
+{
+  if (value < root->value)
+  {
+    root->leftChild = deleteBST(root->leftChild, value);
+  }
+  else if (value > root->value)
+  {
+    root->rightChild = deleteBST(root->rightChild, value);
+  }
+  else
+  {
+    // implemantation of 3 cases
+    if (root->leftChild == NULL)
+    { // case-1 and case-2
+      TreeNode *temp = root->rightChild;
       free(root);
       return temp;
-
-    }else if(root->rightChild==NULL){ //case -2
-      TreeNode*temp=root->leftChild;
+    }
+    else if (root->rightChild == NULL)
+    { // case -2
+      TreeNode *temp = root->leftChild;
       free(root);
       return temp;
-
-    }else{ //case-3
-      TreeNode*temp=inOderSuccesser(root->rightChild);
-      root->value=temp->value;
-      root->rightChild=deleteBST(root->rightChild,temp->value);
-
+    }
+    else
+    { // case-3
+      TreeNode *temp = inOderSuccesser(root->rightChild);
+      root->value = temp->value;
+      root->rightChild = deleteBST(root->rightChild, temp->value);
     }
     return root;
-
   }
+}
 
-  
+// Zigzag Traversal in BST
 
+void zigzagTraversal(TreeNode *root)
+{
+  stack<TreeNode *> currentLevel;
+  stack<TreeNode *> nextLevel;
+  bool leftToRight = true;
+  currentLevel.push(root);
+
+  while (!currentLevel.empty())
+  {
+    TreeNode *x = currentLevel.top();
+    currentLevel.pop();
+    cout << x->value << " ";
+
+    if (leftToRight)
+    {
+      if (x->leftChild)
+      {
+        nextLevel.push(x->leftChild);
+      }
+      if (x->rightChild)
+      {
+        nextLevel.push(x->rightChild);
+      }
+    }
+    else
+    {
+      if (x->rightChild)
+      {
+        nextLevel.push(x->rightChild);
+      }
+      if (x->leftChild)
+      {
+        nextLevel.push(x->leftChild);
+      }
+    }
+
+    if (currentLevel.empty())
+    {
+      leftToRight != leftToRight;
+      swap(currentLevel, nextLevel);
+    }
+  }
 }
 
 int main()
@@ -125,8 +174,10 @@ int main()
   cout << traversal;
   cout << endl;
 
-  int key;
-  cin >> key;
+  zigzagTraversal(root);
+
+  // int key;
+  // cin >> key;
   // if (searchBST(root, key) == NULL)
   // {
   //   cout <<endl<< "Value does not exits in the BST";
@@ -136,18 +187,17 @@ int main()
   //   cout <<endl<< "Value exits in The BST";
   // }
 
-  root=deleteBST(root,key);
-  string afterDelation = "";
-  inOderTraversal(root, afterDelation);
-  cout << afterDelation;
-  cout<<endl;
-
+  // root = deleteBST(root, key);
+  // string afterDelation = "";
+  // inOderTraversal(root, afterDelation);
+  // cout << afterDelation;
+  // cout << endl;
 
   return 0;
 }
 
 /*
 input:
-9
-11 5 9 43 34 1 2 7 21
+10
+11 5 9 43 34 1 2 7 8 21
 */
